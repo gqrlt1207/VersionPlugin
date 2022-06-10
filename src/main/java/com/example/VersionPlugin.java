e1b4fb4f6cc855d1490cb5cdc8bc0b0c442c4f6e
Bill Gao <bill.gao@onespan.com>
2022-06-10 16:40:20 -0400

    feat: adding README and fix plugins ID

---
+++ src/main/java/com/example/VersionPlugin.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class VersionPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        String version = "DEVELOPMENT";
+        try{ 
+            if (project.hasProperty("projectVersion")) {
+                version = project.property("projectVersion").toString(); 
+            } 
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        project.setVersion(version);
+        System.out.println("The project version has been set to " + version);
+    }
+}
--- version-plugin-example/README.md
+++
@@ -1,3 +0,0 @@
-plugins {
-  id "org.xgao.versionplugin" version "1.0"
-}
